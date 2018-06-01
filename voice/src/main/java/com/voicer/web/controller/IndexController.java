48df0de35ef70bf50a6c7ab83cd809c5e4f4c12a
lufyluo <lufyl@163.com>
2018-06-01 13:20:17 +0800

    add web project all

---
+++ voice/src/main/java/com/voicer/web/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.voicer.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping(value = "/index")
+public class IndexController {
+    @RequestMapping
+    public ModelAndView index(ModelAndView mv){
+        mv.setViewName("/index");
+        mv.addObject("name","欢迎使用Thymeleaf!");
+        return mv;
+    }
+    @ResponseBody
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public HashMap<String, Object> get(@RequestParam String name){
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "hello world");
+        map.put("name", name);
+        return map;
+    }
+}
+
