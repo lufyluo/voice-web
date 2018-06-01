7d9c9043fb1d156e6576711bf7684fe25ca2d5a0
lufyluo <lufyl@163.com>
2018-06-01 18:57:13 +0800

    修改JS

--- voice/src/main/resources/static/script/index/index.js
+++ voice/src/main/resources/static/script/index/index.js
@@ -1,25 +1,29 @@
 
-document.ready(function(){
-    if (document.addEventListener){
-        document.addEventListener("keyup",fnKeyup,true);
-    }else{
-        document.attachEvent("onkeyup",fnKeyup);
-    }
+window.onload =function(){
+    alert();
+    // if (document.addEventListener){
+    //     document.addEventListener("keyup",fnKeyup,true);
+    // }else{
+    //     document.attachEvent("onkeyup",fnKeyup);
+    // }
     function fnKeyup(e){
         if(e.keyCode==32)
         {
-            //do something
+            alert("ok");
         }
     }
 
     layui.use('form', function(){
         var form = layui.form;
-
-        //监听提交
-        form.on('submit(formDemo)', function(data){
-            layer.msg(JSON.stringify(data.field));
-            return false;
-        });
+        var $ = layui.jquery
+        $(document).on("onkeyup","#search-input",fnKeyup)
     });
-});
+
+    new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue.js!'
+        }
+    })
+};
 
