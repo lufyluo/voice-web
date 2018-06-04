a20329d09940fd0402b9cfa224d3dbd365b7a7a1
lufyluo@gmail.com <l87289610lufy@DESKTOP-2H9702M>
2018-06-04 23:01:59 +0800

    modify script

--- voice/src/main/resources/static/script/index/index.js
+++ voice/src/main/resources/static/script/index/index.js
@@ -1,39 +1,34 @@
-window.onload = function () {
-    var fnKeyup = function (e) {
-        if (e.keyCode == 32) {
-            alert("ok");
-        }
-    }
-    layui.use(['jquery', 'layer', 'form'], function () {
-        var $ = layui.$ //重点处
-            , layer = layui.layer;
-        var form = layui.form;
-
-        var vm = new Vue({
-            el: '#vue_det',
-            data: {
-                site: "菜鸟教程",
-                url: "www.runoob.com",
-                alexa: "10000"
-            },
-            methods: {
-                details: function() {
-                    return  this.site + " - 学的不仅是技术，更是梦想！";
-                }
+$(function () {
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            searchInput: "",
+            voiceItems: []
+        },
+        methods: {
+            details: function () {
+                return this.site + " - 学的不仅是技术，更是梦想！";
             }
-        })
+        }
+    })
 
+    layui.use(['jquery', 'layer', 'form'], function () {
+        var form = layui.form;
         $(document).keydown(function (event) {
             if (event.keyCode == 13) {
-                $
+                $.ajax({
+                    url: "index/search",
+                    data: vm.data.searchInput,
+                    success: function (data) {
+                        if (data.Success) {
+                            vm.data.voiceItems = data.items;
+                        }
+                    },
+                    dataType: "json"
+                });
             }
         });
     });
-    new Vue({
-        el: '#app',
-        data: {
-            message: 'Hello Vue.js!'
-        }
-    })
-};
+
+})
 
